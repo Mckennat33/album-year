7fb74e660fb567ae79e3a71e8881c5c65a1f6621
tom mckenna <thomasmckenna12@gmail.com>
2025-05-26 17:33:53 -0500

    grabbing value of users email to subscrribe

--- JS/emailService.js
+++ JS/emailService.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const emailService = () => {
         // grab the users email
+        const emailId = document.getElementById('emailInput')
+        emailId.value
+        const subscribeBttn = document.querySelector('#sub-bttn')
+        subscribeBttn.addEventListener('click', (ev) => {
+            ev.preventDefualt()
+            console.log(emailId)
+        })
         
     }
+
+    emailService()
 })

428d8f6704c32451fdcc40865c526eb41007ae1b
tom mckenna <thomasmckenna12@gmail.com>
2025-05-27 19:29:58 -0500

    bugfix: moved email value to the correct scope

--- JS/emailService.js
+++ JS/emailService.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const emailService = () => {
         // grab the users email
-        const emailId = document.getElementById('emailInput')
-        emailId.value
+        
         const subscribeBttn = document.querySelector('.sub-bttn')
         subscribeBttn.addEventListener('click', (ev) => {
+            const emailId = document.getElementById('emailInput').value
             // ev.preventDefualt()
             console.log(emailId)
             // need to get real URL 
             const emailData = 'ursl for data'
-            fetch(emailData, {
-                method: 'POST', 
-                headers: {
-                    "Content-Type": 'application/json', 
-                    body: JSON.stringify({
+            // fetch(emailData, {
+            //     method: 'POST', 
+            //     headers: {
+            //         "Content-Type": 'application/json', 
+            //         body: JSON.stringify({
 
-                    })
-                }
-            }
-            )
+            //         })
+            //     }
+            // }
+            // )
         })
         
     }
